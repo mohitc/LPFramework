02b93a75fac90c3b1e505b2f81a96becebb8369e
Mohit Chamania <mohitc@live.de>
2020-03-06 01:03:02 +0100

    Fix tests to include checks on objective function

--- cplex-solver/src/test/kotlin/com/lpapi/solver/sample/CplexPrimitiveSolverSampleTest.kt
+++ cplex-solver/src/test/kotlin/com/lpapi/solver/sample/CplexPrimitiveSolverSampleTest.kt
@@ -1,11 +1,13 @@
 package com.lpapi.solver.sample
 
 import com.lpapi.model.LPModel
-import com.lpapi.solver.cplex.CplexLpSolver
 import com.lpapi.model.enums.LPSolutionStatus
+import com.lpapi.solver.cplex.CplexLpSolver
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class CplexPrimitiveSolverSampleTest: PrimitiveSolverSample() {
 
@@ -28,9 +30,11 @@ class CplexPrimitiveSolverSampleTest: PrimitiveSolverSample() {
   fun testSolver() {
     val model = initAndSolveModel(initLpModel())
     assertNotNull(model, "Model should be computed successfully.")
+    val objectiveVal = model.solution?.objective ?: 0.0
+    assertTrue(abs(objectiveVal - 2.0) <= 1E-5, "Objective function should be = 2")
     assertEquals(model.variables.get("X")?.result, 1, "X should be = 1")
     assertEquals(model.variables.get("Y")?.result, 1, "Y should be = 1")
-    assertEquals(model.variables.get("Z")?.result, 0, "Z should be = 1")
+    assertEquals(model.variables.get("Z")?.result, 0, "Z should be = 0")
   }
 
 }
