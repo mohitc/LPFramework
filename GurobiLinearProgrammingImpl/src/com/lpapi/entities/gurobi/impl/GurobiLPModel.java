bdec266ca4517a0197d76bb867c418fa94d862c9
mohitc <mohitc@live.de>
2024-12-25 17:51:46 +0100

    Interface and implementation to populate parameters related to model solutions

--- GurobiLinearProgrammingImpl/src/com/lpapi/entities/gurobi/impl/GurobiLPModel.java
+++ GurobiLinearProgrammingImpl/src/com/lpapi/entities/gurobi/impl/GurobiLPModel.java
@@ -4,7 +4,10 @@ import com.lpapi.entities.*;
 import com.lpapi.exception.LPModelException;
 import gurobi.*;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GurobiLPModel extends LPModel<GRBModel, GRBVar, GRBConstr> {
 
@@ -16,10 +19,13 @@ public class GurobiLPModel extends LPModel<GRBModel, GRBVar, GRBConstr> {
 
   private LPConstraintFactory<GRBConstr> grbConstraintFactory;
 
+  private Map<LPSolutionParams, Object> solnParams;
+
   public GurobiLPModel(String identifier) throws LPModelException {
     super(identifier);
     grbVarFactory = new GurobiLPVarFactory();
     grbConstraintFactory = new GurobiLPConstraintFactory();
+    solnParams = new HashMap<>();
   }
 
   @Override
@@ -78,12 +84,56 @@ public class GurobiLPModel extends LPModel<GRBModel, GRBVar, GRBConstr> {
   @Override
   public void computeModel() throws LPModelException {
     try {
+      long startTime = System.currentTimeMillis();
       model.optimize();
-      //TODO set result values to the corresponding variables
+      long endTime = System.currentTimeMillis();
+      int optimstatus = model.get(GRB.IntAttr.Status);
+      switch (optimstatus) {
+        case GRB.Status.OPTIMAL:
+          log.info("Optimal Solution found. Optimal objective: " + model.get(GRB.DoubleAttr.ObjVal) + ", MIP Gap: " + model.get(GRB.DoubleAttr.MIPGap));
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.OPTIMAL);
+          solnParams.put(LPSolutionParams.OBJECTIVE, model.get(GRB.DoubleAttr.ObjVal));
+          solnParams.put(LPSolutionParams.MIP_GAP, model.get(GRB.DoubleAttr.MIPGap));
+          solnParams.put(LPSolutionParams.TIME, (endTime - startTime));
+          break;
+        case GRB.Status.INF_OR_UNBD:
+          log.info("Model is infeasible or unbounded");
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.INFEASIBLE_OR_UNBOUNDED);
+          break;
+        case GRB.Status.INFEASIBLE:
+          log.info("Model is infeasible");
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.INFEASIBLE);
+          break;
+        case GRB.Status.UNBOUNDED:
+          log.info("Model is unbounded");
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.UNBOUNDED);
+          break;
+        case GRB.Status.TIME_LIMIT:
+          log.info("Time limit reached");
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.TIME_LIMIT);
+          solnParams.put(LPSolutionParams.TIME, (endTime - startTime));
+          break;
+        case GRB.Status.CUTOFF:
+          log.info("Cutoff reached. Objective: " + model.get(GRB.DoubleAttr.ObjVal) + ", MIP Gap: " + model.get(GRB.DoubleAttr.MIPGap));
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.CUTOFF);
+          solnParams.put(LPSolutionParams.OBJECTIVE, model.get(GRB.DoubleAttr.ObjVal));
+          solnParams.put(LPSolutionParams.MIP_GAP, model.get(GRB.DoubleAttr.MIPGap));
+          solnParams.put(LPSolutionParams.TIME, (endTime - startTime));
+          break;
+        default:
+          log.info("Optimization was stopped with unknown status (GRB Status: " + optimstatus + ")");
+          solnParams.put(LPSolutionParams.STATUS, LPSolutionStatus.UNKNOWN);
+          break;
+      }
     } catch (GRBException e) {
       log.error("Error in optimizing model", e);
       throw new LPModelException("Error in optimizing model");
     }
+  }
+
+  @Override
+  protected Map<LPSolutionParams, Object> getModelSolutionParams() {
+    return Collections.unmodifiableMap(solnParams);
   }
 
   private GRBLinExpr generateLinearExpression(LPExpression expression) throws LPModelException {
---
+++ LinearProgrammingApi/src/com/lpapi/entities/LPSolutionStatus.java
@@ -0,0 +1,12 @@
+package com.lpapi.entities;
+
+
+public enum LPSolutionStatus {
+  OPTIMAL,
+  INFEASIBLE,
+  UNBOUNDED,
+  TIME_LIMIT,
+  INFEASIBLE_OR_UNBOUNDED,
+  CUTOFF,
+  UNKNOWN;
+}
