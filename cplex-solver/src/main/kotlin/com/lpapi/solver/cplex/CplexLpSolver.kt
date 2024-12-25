834f6ad3f28c936dea4e0e137e1a5c89a6c88688
mohitc <mohitc@live.de>
2024-12-25 17:57:06 +0100

    Initial implementation of the Cplex Solver model

--- cplex-solver/src/main/kotlin/com/lpapi/solver/cplex/CplexLpSolver.kt
+++ cplex-solver/src/main/kotlin/com/lpapi/solver/cplex/CplexLpSolver.kt
@@ -3,13 +3,18 @@ package com.lpapi.solver.cplex
 import com.lpapi.model.LPConstraint
 import com.lpapi.model.LPExpression
 import com.lpapi.model.LPModel
+import com.lpapi.model.LPModelResult
 import com.lpapi.model.enums.LPObjectiveType
 import com.lpapi.model.enums.LPOperator
+import com.lpapi.model.enums.LPSolutionStatus
 import com.lpapi.model.enums.LPVarType
 import com.lpapi.solver.LPSolver
-import com.lpapi.model.enums.LPSolutionStatus
-import ilog.concert.*
+import ilog.concert.IloConstraint
+import ilog.concert.IloLinearNumExpr
+import ilog.concert.IloNumVar
+import ilog.concert.IloNumVarType
 import ilog.cplex.IloCplex
+import kotlin.system.measureTimeMillis
 
 class CplexLpSolver(model: LPModel): LPSolver<IloCplex>(model) {
 
@@ -18,6 +23,8 @@ class CplexLpSolver(model: LPModel): LPSolver<IloCplex>(model) {
   private var variableMap : MutableMap<String, IloNumVar> = mutableMapOf()
 
   private var constraintMap : MutableMap<String, IloConstraint> = mutableMapOf()
+
+  private var cplexObjective: IloLinearNumExpr? = null
 
 
   override fun initModel(): Boolean {
@@ -37,8 +44,60 @@ class CplexLpSolver(model: LPModel): LPSolver<IloCplex>(model) {
   }
 
   override fun solve(): LPSolutionStatus {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    try {
+      log.info { "Starting computation of model" }
+      val executionTime = measureTimeMillis {
+        cplexModel?.solve()
+      }
+      val solutionStatus = getSolutionStatus(cplexModel?.status)
+      log.info { "Computation terminated. Solution Status : $solutionStatus" }
+      if (solutionStatus == LPSolutionStatus.ERROR) {
+        log.error { "Could not determine solution status" }
+        model.solution = LPModelResult(LPSolutionStatus.ERROR)
+      } else if (!(solutionStatus==LPSolutionStatus.INFEASIBLE ||
+              solutionStatus==LPSolutionStatus.UNBOUNDED ||
+              solutionStatus==LPSolutionStatus.INFEASIBLE_OR_UNBOUNDED)) {
+        //add model results
+        extractResults()
+        model.solution = LPModelResult(solutionStatus, cplexModel?.getValue(cplexObjective), executionTime, cplexModel?.mipRelativeGap)
+        log.info { "${model.solution}" }
+      } else {
+       model.solution = LPModelResult(solutionStatus)
+      }
+      return solutionStatus
+    } catch (e: Exception) {
+      log.error { "Error while computing CPLEX model: $e" }
+      model.solution = LPModelResult(LPSolutionStatus.ERROR)
+      return LPSolutionStatus.ERROR
+    }
   }
+
+  /** Function to extract the results of the LP model into the corresponding variables in the model
+   */
+  fun extractResults() {
+    log.info {"Extracting results of computed model into the variables"}
+    variableMap.entries.forEach { entry ->
+      try {
+        cplexModel?.getValue(entry.value)?.let { doubleVal -> model.variables.get(entry.key)?.populateResult(doubleVal) }
+      } catch (e: Exception) {
+        log.error { "Error while extracting results from CPLEX model : $e" }
+      }
+    }
+  }
+
+  fun getSolutionStatus(cplexStatus: IloCplex.Status?): LPSolutionStatus {
+    return when (cplexStatus) {
+      IloCplex.Status.Optimal -> LPSolutionStatus.OPTIMAL
+      IloCplex.Status.InfeasibleOrUnbounded -> LPSolutionStatus.INFEASIBLE_OR_UNBOUNDED
+      IloCplex.Status.Infeasible -> LPSolutionStatus.INFEASIBLE
+      IloCplex.Status.Unbounded -> LPSolutionStatus.UNBOUNDED
+      IloCplex.Status.Bounded -> LPSolutionStatus.BOUNDED
+      IloCplex.Status.Error -> LPSolutionStatus.ERROR
+      null -> LPSolutionStatus.ERROR
+      else -> { LPSolutionStatus.UNKNOWN}
+    }
+  }
+
 
   /** Function to initialize all variables in the model
    *
@@ -63,9 +122,11 @@ class CplexLpSolver(model: LPModel): LPSolver<IloCplex>(model) {
   override fun initObjectiveFunction(): Boolean {
     log.info { "Initializing Objective Function" }
     try {
+      //Initialize cplex objective, to be used later to extract value of the objective function
+      cplexObjective = generateExpression(model.objective.expression)
       when(model.objective.objective) {
-        LPObjectiveType.MAXIMIZE -> cplexModel?.maximize(generateExpression(model.objective.expression))
-        LPObjectiveType.MINIMIZE -> cplexModel?.minimize(generateExpression(model.objective.expression))
+        LPObjectiveType.MAXIMIZE -> cplexModel?.maximize(cplexObjective)
+        LPObjectiveType.MINIMIZE -> cplexModel?.minimize(cplexObjective)
         else -> {
           log.error { "Mechanism not implemented to support objective type ${model.objective.objective}" }
           null
@@ -92,17 +153,17 @@ class CplexLpSolver(model: LPModel): LPSolver<IloCplex>(model) {
         }
 
         val modelConstraint : IloConstraint? =
-          when(lpConstraint.operator) {
-            LPOperator.LESS_EQUAL -> cplexModel?.addLe(generateExpression(lpConstraint.lhs), generateExpression(lpConstraint.rhs), lpConstraint.identifier)
-            LPOperator.GREATER_EQUAL -> cplexModel?.addGe(generateExpression(lpConstraint.lhs), generateExpression(lpConstraint.rhs), lpConstraint.identifier)
-            LPOperator.EQUAL -> cplexModel?.addEq(generateExpression(lpConstraint.lhs), generateExpression(lpConstraint.rhs), lpConstraint.identifier)
-            else -> {null}
-          }
+            when(lpConstraint.operator) {
+              LPOperator.LESS_EQUAL -> cplexModel?.addLe(generateExpression(lpConstraint.lhs), generateExpression(lpConstraint.rhs), lpConstraint.identifier)
+              LPOperator.GREATER_EQUAL -> cplexModel?.addGe(generateExpression(lpConstraint.lhs), generateExpression(lpConstraint.rhs), lpConstraint.identifier)
+              LPOperator.EQUAL -> cplexModel?.addEq(generateExpression(lpConstraint.lhs), generateExpression(lpConstraint.rhs), lpConstraint.identifier)
+              else -> {null}
+            }
         if (modelConstraint==null) {
           log.error { "Error while generating model constraint ${lpConstraint.identifier}" }
           return false
         }
-        constraintMap.put(lpConstraint.identifier, modelConstraint)
+        constraintMap[lpConstraint.identifier] = modelConstraint
       } catch (e: Exception) {
         log.error { "Error while initializing CPLEX Constraint ($lpConstraint) : $e" }
         return false
--- cplex-solver/src/test/kotlin/com/lpapi/solver/cplex/CplexLpSolverConverterTest.kt
+++ cplex-solver/src/test/kotlin/com/lpapi/solver/cplex/CplexLpSolverConverterTest.kt
@@ -1,8 +1,11 @@
 package com.lpapi.solver.cplex
 
 import com.lpapi.model.LPModel
+import com.lpapi.model.enums.LPSolutionStatus
 import com.lpapi.model.enums.LPVarType
+import ilog.cplex.IloCplex
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class CplexLpSolverConverterTest {
@@ -12,6 +15,27 @@ class CplexLpSolverConverterTest {
     val solver = CplexLpSolver(LPModel())
     LPVarType.values().forEach { v ->
       assertNotNull(solver.getCplexVarType(v), "No equivalent variable type associated with LPVarType $v")
+    }
+  }
+
+  @Test
+  fun testGetCplexSolutionStatus() {
+    val solutionStatusMap: Map<IloCplex.Status?, LPSolutionStatus> = mapOf(
+        Pair(IloCplex.Status.Optimal, LPSolutionStatus.OPTIMAL),
+        Pair(IloCplex.Status.Unbounded, LPSolutionStatus.UNBOUNDED),
+        Pair(IloCplex.Status.Infeasible, LPSolutionStatus.INFEASIBLE),
+        Pair(IloCplex.Status.InfeasibleOrUnbounded, LPSolutionStatus.INFEASIBLE_OR_UNBOUNDED),
+        Pair(IloCplex.Status.Bounded, LPSolutionStatus.BOUNDED),
+        Pair(IloCplex.Status.Error, LPSolutionStatus.ERROR),
+        Pair(IloCplex.Status.Feasible, LPSolutionStatus.UNKNOWN),
+        Pair(IloCplex.Status.Unknown, LPSolutionStatus.UNKNOWN),
+        Pair(null, LPSolutionStatus.ERROR)
+    )
+
+    val solver = CplexLpSolver(LPModel())
+    solutionStatusMap.entries.forEach { entry ->
+      assertEquals(entry.value, solver.getSolutionStatus(entry.key),
+          "CPLEX status ${entry.key} not translated correctly to ${entry.value}")
     }
   }
 }
